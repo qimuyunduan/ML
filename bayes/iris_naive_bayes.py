9d421aabcd4c54e02575e78cca4c8d86b7fef72c
qimuyunduan <84607842@qq.com>
2016-03-29 15:36:41 +0800

    iris_navie

--- bayes/iris_naive_bayes.py
+++ bayes/iris_naive_bayes.py
@@ -1,20 +1,14 @@
 
 #coding=utf-8
-
-
 from sklearn import datasets
 iris = datasets.load_iris()
-iris.data[:5]
-#array([[ 5.1,  3.5,  1.4,  0.2],
-#       [ 4.9,  3. ,  1.4,  0.2],
-#       [ 4.7,  3.2,  1.3,  0.2],
-#       [ 4.6,  3.1,  1.5,  0.2],
-#       [ 5. ,  3.6,  1.4,  0.2]])
 
-#我们假定sepal length, sepal width, petal length, petal width 4个量独立且服从高斯分布，用贝叶斯分类器建模
+print iris.data[:5]
+#用贝叶斯分类器建模
 from sklearn.naive_bayes import GaussianNB
 gnb = GaussianNB()
-y_pred = gnb.fit(iris.data, iris.target).predict(iris.data)
-right_num = (iris.target == y_pred).sum()
+#用原始数据进行拟合
+result_pred = gnb.fit(iris.data, iris.target).predict(iris.data)
+right_num = (iris.target == result_pred).sum()
 print("Total testing num :%d , naive bayes accuracy :%f" %(iris.data.shape[0], float(right_num)/iris.data.shape[0]))
-# Total testing num :150 , naive bayes accuracy :0.960000
+
